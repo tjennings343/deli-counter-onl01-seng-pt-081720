09b2575b737e65c7e4946a9c8e76b76e56bc55d8
Trey Jennings <tjennings343@gmail.com>
2020-08-27 00:48:50 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,6 +18,6 @@ def now_serving(katz_deli)
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli[0]}."
-    katz_deli.shift
   end
+  katz_deli.shift
 end
